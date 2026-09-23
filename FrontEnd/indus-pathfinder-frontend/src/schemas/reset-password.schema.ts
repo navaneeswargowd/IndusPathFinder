60e2020fb93cadddf8f8281f54bb2803073922d3
navaneeswargowd <manoj.d@orchasp.com>
2026-09-23 13:14:59 +0530

    Comment out password validation schema
    Commented out the password validation schema for reset password.

--- FrontEnd/indus-pathfinder-frontend/src/schemas/reset-password.schema.ts
+++ FrontEnd/indus-pathfinder-frontend/src/schemas/reset-password.schema.ts
@@ -6,29 +6,29 @@ import {
   PASSWORD_REGEX,
 } from "../utils/validation.utils";
 
-const passwordSchema =
-  z
-    .string()
-    .min(
-      8,
-      "Password must contain at least 8 characters"
-    )
-    .regex(
-      /[A-Z]/,
-      "Password must contain at least one uppercase letter"
-    )
-    .regex(
-      /[a-z]/,
-      "Password must contain at least one lowercase letter"
-    )
-    .regex(
-      /[0-9]/,
-      "Password must contain at least one number"
-    )
-    .regex(
-      /[^A-Za-z0-9]/,
-      "Password must contain at least one special character"
-    );
+// const passwordSchema =
+//   z
+//     .string()
+//     .min(
+//       8,
+//       "Password must contain at least 8 characters"
+//     )
+//     .regex(
+//       /[A-Z]/,
+//       "Password must contain at least one uppercase letter"
+//     )
+//     .regex(
+//       /[a-z]/,
+//       "Password must contain at least one lowercase letter"
+//     )
+//     .regex(
+//       /[0-9]/,
+//       "Password must contain at least one number"
+//     )
+//     .regex(
+//       /[^A-Za-z0-9]/,
+//       "Password must contain at least one special character"
+//     );
 
 export const resetPasswordSchema =
   z
